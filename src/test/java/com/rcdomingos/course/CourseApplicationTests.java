de47e625fe304a8b6578524d66ed96f91b54217d
Reginaldo C Domingos <rcdomingos@live.com>
2021-06-30 19:27:58 -0300

    Project created

---
+++ src/test/java/com/rcdomingos/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcdomingos.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
